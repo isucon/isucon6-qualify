a4df99892d147087e8ff7b9f19e7ef303daf5573
oprstchn <tyakachika@gmail.com>
2018-08-19 14:52:08 +0900

    add queryUnescape

--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -263,10 +263,13 @@ func keywordByKeywordHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	keyword := mux.Vars(r)["keyword"]
+	keyword, err := url.QueryUnescape(mux.Vars(r)["keyword"])
+	if err != nil {
+		return
+	}
 	row := db.QueryRow(`SELECT * FROM entry WHERE keyword = ?`, keyword)
 	e := Entry{}
-	err := row.Scan(&e.ID, &e.AuthorID, &e.Keyword, &e.Description, &e.UpdatedAt, &e.CreatedAt)
+	err = row.Scan(&e.ID, &e.AuthorID, &e.Keyword, &e.Description, &e.UpdatedAt, &e.CreatedAt)
 	if err == sql.ErrNoRows {
 		notFound(w)
 		return
